441484beb579841e207cff787ca98ffeb2026f98
marcus <linfaimom@gmail.com>
2016-07-25 12:15:18 +0800

    注释

--- app/src/main/java/com/example/marcus/jokecollections/PresenterImp.java
+++ app/src/main/java/com/example/marcus/jokecollections/PresenterImp.java
@@ -8,12 +8,15 @@ import java.util.List;
  * Created by marcus on 16/7/25.
  */
 public class PresenterImp implements PresenterItf {
-
+    //中介人必须认识双方
     private MainActivityItf mainActivityItf;
     private ModelItf modelItf;
 
+
     public PresenterImp(MainActivityItf mainActivityItf) {
+        //持有对 View 的引用
         this.mainActivityItf = mainActivityItf;
+        //View 不认识 Model，所以这里的实例化由 Presenter 完成
         this.modelItf = new ModelImp();
     }
 
